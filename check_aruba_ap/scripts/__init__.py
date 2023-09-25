dba5314e5c5125b6cb4b7fed38ae0fff810360a1
Benjamin Renard <brenard@easter-eggs.com>
2023-09-25 16:04:16 +0200

    Remove default value of -H and make it required and add default values of -C & -V in usage message

--- check_aruba_ap/scripts/__init__.py
+++ check_aruba_ap/scripts/__init__.py
@@ -12,10 +12,18 @@ def get_parser(*args, **kwargs):
         "--hostname",
         type=str,
         help="Aruba SNMP hostname (IP address required for the current elected virtual controller)",
-        default="localhost",
+        required=True,
     )
-    parser.add_argument("-C", "--snmp-community", type=str, help="SNMP community", default="public")
-    parser.add_argument("-V", "--snmp-version", type=int, help="SNMP version", default=1)
+    parser.add_argument(
+        "-C",
+        "--snmp-community",
+        type=str,
+        help="SNMP community (default: public)",
+        default="public",
+    )
+    parser.add_argument(
+        "-V", "--snmp-version", type=int, help="SNMP version (default: 1)", default=1
+    )
     parser.add_argument(
         "-cw",
         "--warning-cpu-threshold",
